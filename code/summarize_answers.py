8b8e4673baaec654307aac53410fb47fc963cc0e
jeandeluge <jeandelugedev@gmail.com>
2024-10-17 02:47:19 +0000

    fix: miss matched summary

--- code/summarize_answers.py
+++ code/summarize_answers.py
@@ -16,30 +16,22 @@ class SummerizeAnswers:
         self.tokenizer = PreTrainedTokenizerFast.from_pretrained('digit82/kobart-summarization')
         self.model = BartForConditionalGeneration.from_pretrained('digit82/kobart-summarization')
     
-    def get_original_lines(self):
-        with open(self.filename) as f:
-            for line in f:
-                original_line = json.loads(line)
-                query = original_line["content"]
-                yield query
-
-    def summerize_line(self):
-        for original_line in self.get_original_lines():
-            inputs = self.tokenizer.encode(original_line, return_tensors="pt")
-            input_ids = torch.cat([torch.tensor([[self.tokenizer.bos_token_id]]), inputs, torch.tensor([[self.tokenizer.eos_token_id]])], dim=1)
-            
-            with torch.no_grad():
-                summary_ids = self.model.generate(input_ids, num_beams=4, max_length=300, eos_token_id=self.tokenizer.eos_token_id)
-            summarized_line = self.tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
-            yield summarized_line
+    def summerize_line(self, query):
+        inputs = self.tokenizer.encode(query, return_tensors="pt")
+        input_ids = torch.cat([torch.tensor([[self.tokenizer.bos_token_id]]), inputs, torch.tensor([[self.tokenizer.eos_token_id]])], dim=1)
+        
+        with torch.no_grad():
+            summary_ids = self.model.generate(input_ids, num_beams=4, max_length=300, eos_token_id=self.tokenizer.eos_token_id)
+        summarized_line = self.tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
+        return summarized_line
 
     def save_line_to_file(self):
-        new_content = []
 
-        with open(self.filename, 'r', encoding='utf-8') as f:
+        with open(self.filename, 'r', encoding='utf-8') as f, open(self.savefilename, 'w', encoding='utf-8') as new_file:
             for original_line in f:
                 original_content = json.loads(original_line)
-                summary = next(self.summerize_line())
+                query_original_content = original_content["content"]
+                summary = self.summerize_line(query_original_content)
 
                 combined_dict = {
                 "docid": original_content['docid'],
@@ -47,12 +39,8 @@ class SummerizeAnswers:
                 "content": original_content['content'],
                 "summary": summary
                 }
-                new_content.append(combined_dict)
 
-        with open(self.savefilename, 'w', encoding='utf-8') as new_file:
-            for content in new_content:
-                json.dump(content, new_file, ensure_ascii=False)
-                new_file.write('\n')
+                new_file.write(f'{json.dumps(combined_dict, ensure_ascii=False)}\n')
 
 def __main__():
     summerize = SummerizeAnswers("../../../data/documents.jsonl")
